e4db5d596b716340aba7e8bdd4b22b7a1ece69ea
Patrick <alip@uni.coventry.ac.uk>
2020-02-06 21:07:27 +0000

    Running on Python Flask

--- routes.py
+++ routes.py
@@ -6,9 +6,11 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return flask.render_template('index.html')
+    #return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run() 
+if __name__ == '__main__':
+    app.run() 
 
 #Login API
 #Editor API
